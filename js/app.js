2c35554c2040f63e33815b27adef103baa99e400
alvaro valencia <alvaroleandro74@gmail.com>
2022-03-18 09:15:40 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// function tomaFecha(){
+// let dato6 = document.getElementById("dia").value;
+// let dato7 = document.getElementById("hora").value;
+
+// const j = [dato6,dato7]
+// console.log(j)
+// }
+
+// if (x == false) {
+//     console.log('es falso ')
+//     x  = true;
+// }else{
+//     console.log('es verdadero')
+//     x = false;
+// }
+let x = Boolean();
+
+function testigo(){
+    
+}
+
+function tomaDatos() {
+    let dato1 = document.getElementById("fname").value;
+    let dato2 = document.getElementById("apellido").value;
+    let dato3 = document.getElementById("dni").value;
+    let dato4 = document.getElementById("email").value;
+    let dato5 = document.getElementById("socio").value;
+    let allData = [ dato1 , dato2 , dato3 , dato4 , dato5 ];
+}
+
+switch (x) {
+    case true:
+        console.log('es verdadero');
+        X = false; 
+    break;
+
+    case false:
+        console.log('es falso');
+        x = true;
+    break;
+    default: console.log('esperando a que se ingresen los datos')
+}
+
+
