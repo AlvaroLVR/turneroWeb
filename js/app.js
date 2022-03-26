5adba0a8c10fee2b1802353771257c16dcc82ec8
alvaro valencia <alvaroleandro74@gmail.com>
2022-03-26 18:44:57 -0300

    test

--- js/app.js
+++ js/app.js
@@ -1,27 +1,23 @@
 //declaracion de variables globales 
-let registroSocio = [] ; 
-let c = 0;
-// Crear una clase constructora para tomar los datos de los socios 
+let c = 0; 
+let partnerSchedule = []; 
+// Crear una Objeto los datos de los socios 
 class Socio{
-    constructor(nombre,apellido,dni,email,nSocio){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.email = email;
-        this.nSocio = nSocio;
+    constructor(nombre,apellido,dni,email,nSocio,nTurno){
+        this.nombre = nombre.toUpperCase();
+        this.apellido = apellido.toUpperCase();
+        this.dni = parseFloat(dni);
+        this.email = email.toUpperCase();
+        this.nSocio = nSocio;          // si se parsea ignora los 0 por delante, ej 00990011 parseado = 990011. hay obras sociales que tiene como numero de socio el 00 por delante. 
+        this.nTurno = nTurno;
+        //this.fechaIng = fechaIng; Fecha de ingreso del turno 
     }
 }
 // toma los datos del socio 
 function tomaDatos() {
-    c++; 
-    let dato1 = document.getElementById("fname").value;
-    let dato2 = document.getElementById("apellido").value;
-    let dato3 = document.getElementById("dni").value;
-    let dato4 = document.getElementById("email").value;
-    let dato5 = document.getElementById("socio").value;
+    c++;  // contador el numero de turno del socio ingresado
 
-    let registroSocio = new  Socio( dato1 , dato2 , dato3 , dato4 , dato5 );
-    
-    alert(registroSocio.nombre +' tenes el turno '+ c)
+    partnerSchedule.push(new Socio( document.getElementById("fname").value , document.getElementById("apellido").value , document.getElementById("dni").value , document.getElementById("email").value , document.getElementById("socio").value , c ));
+
+    alert(document.getElementById("fname").value + ' ya esta registrado. Tenes el turno  ' +c )
 }
-
