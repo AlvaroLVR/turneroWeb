ed0a7e9d24e99bda8b9b12056235b84b02d4f2a5
alvaro valencia <alvaroleandro74@gmail.com>
2022-03-29 12:52:48 -0300

    corrigiendo el codigo para la primera entrega del proyecto final

--- js/app.js
+++ js/app.js
@@ -1,23 +1,78 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// FUNCIONALIDADES PARA AGREGAR
+
+//  - comprobante: darle un comprobante del turno al usuario con un codigo para que pueda presentarlo el dia que se le fue asignado.
+
+//  - calendario: agregar un calendario que indique los dias disponibles para el turno 
+
+//  - verificar la entrada de datos
+
+//  - recordatorios: enviar un emial y whatasapp recordatorio del turno,  dia antes del mismo.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      codigo usando getElementById                                             //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //declaracion de variables globales 
-let c = 0; 
-let partnerSchedule = []; 
-// Crear una Objeto los datos de los socios 
-class Socio{
-    constructor(nombre,apellido,dni,email,nSocio,nTurno){
-        this.nombre = nombre.toUpperCase();
-        this.apellido = apellido.toUpperCase();
-        this.dni = parseFloat(dni);
-        this.email = email.toUpperCase();
-        this.nSocio = nSocio;          // si se parsea ignora los 0 por delante, ej 00990011 parseado = 990011. hay obras sociales que tiene como numero de socio el 00 por delante. 
-        this.nTurno = nTurno;
-        //this.fechaIng = fechaIng; Fecha de ingreso del turno 
+// let c = 0; 
+// let partnerSchedule = []; 
+// // Crear un Objeto con los datos de los socios 
+// class Socio{
+//     constructor(nombre,apellido,dni,email,nSocio,nTurno){
+//         this.nombre = nombre.toUpperCase();
+//         this.apellido = apellido.toUpperCase();
+//         this.dni = parseFloat(dni);
+//         this.email = email.toUpperCase();
+//         this.nSocio = nSocio;// si se parsea ignora los 0 por delante, ej 00990011 parseado = 990011. hay obras sociales que tiene como numero de socio el 00 por delante. 
+//         this.nTurno = nTurno;
+//         //this.fechaIng = fechaIng; Fecha de ingreso del turno 
+//     }
+// }
+// // toma los datos del socio 
+// function tomaDatos() {
+//     c++;  // contador el numero de turno del socio ingresado
+
+//     partnerSchedule.push(new Socio( document.getElementById("fname").value , document.getElementById("apellido").value , document.getElementById("dni").value , document.getElementById("email").value , document.getElementById("socio").value , c )); //guarda los datos en las propiedades del objeto Socio y se pushean en el array partnerSchedule
+
+//     alert(document.getElementById("fname").value + ' ya estas registrado. Tenes el turno  ' + c )
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                         codigo sin usar getElementById                                         //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let partnerSchedule = [];  // array de socios  dejarlo por fuera del while para que no se pise y vuelva a valor 0 
+
+while (true) {
+    // tomando datos 
+    let nombre = prompt('ingrese su Nombre');
+    let apellido = prompt('ingrese su Apellido'); 
+    let dni = prompt('ingrese su DNI');
+    let email = prompt('ingrese su email');
+    let numSocio = prompt('ingrese su numero de socio');
+
+    //creo la clse Socio con sus propiedades 
+    class Socio{
+        constructor(nombre,apellido,dni,email,nSocio){
+            this.nombre = nombre.toUpperCase();
+            this.apellido = apellido.toUpperCase();
+            this.dni = parseFloat(dni);
+            this.email = email.toUpperCase();
+            this.nSocio = nSocio;// si se parsea ignora los 0 por delante, ej 00990011 parseado = 990011. hay obras sociales que tiene como numero de socio el 00 por delante. 
+            // this.nTurno = nTurno;
+            //this.fechaIng = fechaIng; Fecha de ingreso del turno 
+        }
     }
-}
-// toma los datos del socio 
-function tomaDatos() {
-    c++;  // contador el numero de turno del socio ingresado
 
-    partnerSchedule.push(new Socio( document.getElementById("fname").value , document.getElementById("apellido").value , document.getElementById("dni").value , document.getElementById("email").value , document.getElementById("socio").value , c ));
+    //guardar los socios en un array 
 
-    alert(document.getElementById("fname").value + ' ya esta registrado. Tenes el turno  ' +c )
+    partnerSchedule.push( new Socio(nombre, apellido,dni, email,numSocio) );
+
+    // mostrar el nombre del socio e informarle que numero de turno tiene. 
+
+    alert(nombre + ' ya estas registrado. Tenes el turno  '+ partnerSchedule.length);  // provisoriamente uso el length del array de socios para saber el numero de turno, mas adelante tendré que ver como saberlo si muchos usuarios se estan registrando y darles el numero correcto del turno. 
 }
